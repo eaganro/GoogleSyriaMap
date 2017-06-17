5ee0af3e1d722905346fc1ecba15437ddb9f0179
Rory Eagan <eaganro@packer-debian-8-amd64.droplet.local>
2017-06-17 13:56:14 -0400

    Fix problems with deleting old maps when quickly switching between them.

--- public/SyriaMap.js
+++ public/SyriaMap.js
@@ -2,7 +2,6 @@ var date;
 var zoom;
 var lat;
 var lang;
-var lowDate = new Date("04-28-2013");
 var dateSelector = document.getElementById('mapDate');
 var datePick = datepicker(document.getElementById('mapDate'), {
   minDate: new Date(2015, 5, 6), // June 1st, 2016. 
@@ -10,7 +9,6 @@ var datePick = datepicker(document.getElementById('mapDate'), {
   formatter: function(el, date) {
     // This will display the date as `1/1/2017`. 
     el.value = date.getFullYear()+'-'+(date.getMonth())+'-'+date.getDate();
-    console.log("please lord: " + el.value);
   }, 
   onSelect: function(instance) {
     // Show which date was selected. 
@@ -33,17 +31,9 @@ var map = new google.maps.Map(document.getElementById('map'), {
 dateSelector.addEventListener('change', dateListener);
 
 function dateListener(){
-  console.log(dateSelector.value);
   var dateParts = `${dateSelector.value}`.split('-');
-  console.log(dateParts);
   dateParts[1] ++;
   date = new Date (dateParts[0],dateParts[1]-1,dateParts[2]);
-  //date = new Date(dateParts[0]+'-'+dateParts[1]+'-'+dateParts[2]);
-  console.log(navigator.userAgent);
-  console.log(date);
-  //if(date > lowDate) changeMap("change");
-  //if(date < lowDate) changeMap("change");
-
   changeMap("change");
 }
 
@@ -71,8 +61,6 @@ document.getElementById('nextMapBut').addEventListener('click', function(){
 });
 
 function changeCenter(){
-  console.log(lat);
-  console.log(lang);
   var latlng = new google.maps.LatLng(lat, lang);
   map.setZoom(zoom);
   map.setCenter(latlng);
@@ -80,26 +68,17 @@ function changeCenter(){
 
 function changeMap(type){
   var data = JSON.stringify({year: date.getFullYear(), month: date.getMonth()+1, day: date.getDate(), type: type});
-  console.log(date.getMonth());
-  console.log(date.getDate());
-  console.log("DATA:" + data);
   var xhr = new XMLHttpRequest();
-  xhr.open("POST", "http://"+document.domain+"/scw/mapUpdate", true);
+  xhr.open("POST", "/scw/mapUpdate", true);
   xhr.setRequestHeader("Content-type","application/json");
   xhr.onreadystatechange = function () {
       if (xhr.readyState == 4 && xhr.status == 200) {
-        console.log(lat);
-          console.log(xhr.responseText);
           var response = JSON.parse(xhr.responseText);
           if(xhr.responseText.includes("https:")){
             initMap(response.mapURL, zoom, lat, lang)
           }else{
             initMap("https:"+response.mapURL, zoom, lat, lang)
           }
-          console.log(lat);
-          console.log(lang);
-          console.log(zoom);
-          console.log(response.mapDate);
           urlExtra.date = response.mapDate.substring(0,10);
           urlExtra.zoom = map.getZoom();
           urlExtra.center = map.getCenter();
@@ -107,16 +86,14 @@ function changeMap(type){
           dateSelector.value = urlExtra.date;
           var dateParts = dateSelector.value.split('-');
           date = new Date (dateParts[0],dateParts[1]-1,dateParts[2]);
-          //date.setDate(date.getDate() +1);
-          console.log(date.getMonth() );
-          console.log(date);
         }
   };
   xhr.send(data);
 }
-var overlayTemp; 
+var overlayTemp = []; 
 function removeOldMap(){
-  overlayTemp.setMap(null);
+  overlayTemp[1].setMap(null);
+  overlayTemp.splice(0,1);
   console.log("-----------------deleted------------")
 }
 
@@ -134,31 +111,17 @@ function getMapInfo(){
   var url = window.location.href.split("/?")[1];
   if(url){
     var urlSplit = url.split(/\/|%2F/);
-    console.log()
-    /*var urlSplit2;
-    if(urlSplit.length == 1){
-      urlSplit2 = urlSplit[0].split(/%2F|%2C/);
-    } else{
-      var urlSplit2 = urlSplit;
-    }*/
-
     var dateParts = urlSplit[0].split('-');
-    console.log(dateParts);
     date = new Date (dateParts[0],dateParts[1]-1,dateParts[2]);
     zoom = parseInt(urlSplit[1]);
     var latlang = urlSplit[2].slice(1,urlSplit[2].length-2).split(/,%20|%2C%20/);
     lat = latlang[0]
     lang = latlang[1]
-    console.log(lat);
-    console.log(lang);
-    console.log(zoom);
     changeMap("change");
     changeCenter();
   }else{
     date = new Date();
     date.setMonth(date.getMonth());
-    console.log(date);
-    console.log(date.getMonth());
     changeMap("change");
   }
 }
@@ -174,8 +137,9 @@ function initMap(url, zooom, latitude, longitude) {
       new google.maps.LatLng(37.653165, 48.931870));
   var srcImage;
   if(typeof url === 'string'){
-    overlayTemp = new google.maps.GroundOverlay();
-    overlayTemp = overlay;
+    tempOver = new google.maps.GroundOverlay();
+    tempOver = overlay;
+    overlayTemp.push(tempOver);
     srcImage = url;
     setTimeout(removeOldMap, 3000);
   }else{
